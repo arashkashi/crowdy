219f456f65c726337d1afbf4c8938121c2f90e7a
Arash <arashkashi@gmail.com>
2013-03-17 22:52:11 -0400

    Before running the dependancy script.

--- bin/crowdy/crowdy.js
+++ bin/crowdy/crowdy.js
@@ -12,12 +12,41 @@ goog.require('lime.animation.Spawn');
 goog.require('lime.animation.FadeTo');
 goog.require('lime.animation.ScaleTo');
 goog.require('lime.animation.MoveTo');
+goog.require('crowdy.Button');
 
+crowdy.WIDTH = 768;
+crowdy.HEIGHT = 1004;
 
 // entrypoint
 crowdy.start = function(){
+	
+	crowdy.director = new lime.Director(document.body, crowdy.WIDTH, crowdy.HEIGHT);
+    crowdy.director.makeMobileWebAppCapable();
 
+	lime.Label.defaultFont = 'Impact';
+    lime.Label.installFont('Impact', 'assets/impact.ttf');
+    crowdy.loadMenuScene();
+}
 
+crowdy.loadMenuScene = function(opt_transition){
+	var scene = new lime.Scene();
+    crowdy.director.replaceScene(scene, opt_transition ? lime.transitions.MoveInDown : undefined);
+
+    var layer = new lime.Layer().setPosition(crowdy.WIDTH * .5, 0);
+    scene.appendChild(layer);
+
+    var title = new lime.Sprite().setFill('assets/main_title.png').setPosition(0, 250);
+    layer.appendChild(title);
+
+	var contents = new lime.Layer().setPosition(0, 280);
+	layer.appendChild(contents);
+	
+	//var btn_play = new crowdy.Button('PLAY NOW').setPosition(0, 330).setSize(250, 100);
+    //contents.appendChild(btn_play);
+    //goog.events.listen(btn_play, lime.Button.Event.CLICK, function() {
+    //  crowdy.loadGame(1);
+    //});
+	
 }
 
 
